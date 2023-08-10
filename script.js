0a6c8272a42f8381af6764e0298f999ea2044d2c
dynstat <vs15vivek@gmail.com>
2023-08-10 21:21:00 +0530

    dot animation inside the box

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 gsap.from(".page1 .box", {
     scale: 0,
-    duration: 3,
+    duration: 10,
+    rotate: 1720
 })
 gsap.from(".page2 .box", {
     scale: 0,
@@ -8,8 +9,21 @@ gsap.from(".page2 .box", {
     scrollTrigger: {
         trigger: ".page2 .box",
         scroller: "body",
-        // markers: true,
-        start: "top 60%"
+        markers: true,
+        start: "top 76%"
 
     }
+})
+gsap.to(".page3 .box", {
+    scale: 0,
+    duration: 3,
+    x: 1000,
+    y: -300,
+    scrollTrigger: {
+        trigger: ".page3 .box",
+        scroller: "body",
+        markers: true,
+        start: "top 76%"
+    }
+
 })
