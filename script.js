f8a430ae95764171b5ae1f5a6402b8e972f60c31
dynstat <vs15vivek@gmail.com>
2023-08-12 22:06:14 +0530

    timeline added

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-gsap.from(".page1 .box", {
+console.log("hello");
+// gsap.from(".page1 .box", {
+//     scale: 0,
+//     duration: 10,
+//     rotate: 1720
+// })
+// gsap.from(".page2 .box", {
+//     scale: 0,
+//     duration: 3,
+//     scrollTrigger: {
+//         trigger: ".page2 .box",
+//         scroller: "body",
+//         markers: true,
+//         start: "top 76%"
+
+//     }
+// })
+// gsap.to(".page3 .box", {
+//     scale: 0,
+//     duration: 3,
+//     x: 1000,
+//     y: -300,
+//     scrollTrigger: {
+//         trigger: ".page3 .box",
+//         scroller: "body",
+//         markers: true,
+//         start: "top 76%",
+//     },
+// })
+
+var tl = gsap.timeline();
+
+tl.from(".page1 .box", {
     scale: 0,
     duration: 10,
     rotate: 1720
 })
-gsap.from(".page2 .box", {
+tl.from(".page2 .box", {
     scale: 0,
     duration: 3,
     scrollTrigger: {
@@ -13,17 +45,4 @@ gsap.from(".page2 .box", {
         start: "top 76%"
 
     }
-})
-gsap.to(".page3 .box", {
-    scale: 0,
-    duration: 3,
-    x: 1000,
-    y: -300,
-    scrollTrigger: {
-        trigger: ".page3 .box",
-        scroller: "body",
-        markers: true,
-        start: "top 76%"
-    }
-
 })
